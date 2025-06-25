793fcf794b81a44e0afd763d079e6f3d3417b4d8
Marshelby <elmarceloxd@gmail.com>
2025-06-25 01:39:55 -0400

    Mejoras visuales: rediseño de tarjetas, nueva tipografía y estructura profesional

--- src/components/Destacados.jsx
+++ src/components/Destacados.jsx
@@ -36,7 +36,7 @@ function Destacados() {
             <p>{item.descripcion}</p>
             <p className="precio">{item.precio}</p>
             <a
-              href={`https://wa.me/56967215364?text=Hola, me interesa el producto destacado: ${item.nombre}`}
+              href={`https://wa.me/56967215364?text=Hola, me interesa el pack: ${item.nombre}`}
               target="_blank"
               rel="noreferrer"
             >
